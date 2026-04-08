87bd1412a89f5ec870a93921b569810daef0e7bc
jexx <jexx200628@gmail.com>
2026-04-08 23:56:37 +0630

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const admin = require('firebase-admin');
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://leebl-ad3ee-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+const db = admin.database();
+
+async function fetchAndUpload() {
+    try {
+        const url = "https://query1.finance.yahoo.com/v8/finance/chart/^SET";
+        const res = await axios.get(url);
+        const meta = res.data.chart.result[0].meta;
+
+        const set = meta.regularMarketPrice;
+        const val = meta.regularMarketVolume;
+
+        // 2D တွက်ခြင်း
+        const sStr = set.toFixed(2).toString();
+        const vStr = val.toString();
+        const twoD = sStr.charAt(sStr.length - 1) + vStr.charAt(vStr.length - 1);
+
+        // Firebase သို့ ပို့ခြင်း
+        await db.ref('live_data').update({
+            last_result: twoD,
+            set: set.toFixed(2),
+            value: (val / 1000000).toFixed(2),
+            full_date: new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' })
+        });
+        console.log(`Updated: ${twoD}`);
+    } catch (e) {
+        console.log("Waiting for Market...");
+    }
+}
+
+setInterval(fetchAndUpload, 5000); // ၅ စက္ကန့်တစ်ခါ
